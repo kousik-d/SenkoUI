351220ea6840772de4f3f56374b9e0eb2403ae7b
Kousik <kousikdasari0475@gmail.com>
2025-08-12 12:52:44 +0530

    feat: added package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "SenkoUI",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v10_15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,7 +19,9 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SenkoUI"),
+            name: "SenkoUI",
+            path: "Sources/SenkoUI"
+        )
 
     ]
 )
